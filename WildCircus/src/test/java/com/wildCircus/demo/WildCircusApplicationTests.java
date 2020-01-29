a98d2e10ddbe14695e2765e5a689a4270698195c
dani452 <52546806+dani452@users.noreply.github.com>
2020-01-29 10:44:18 +0100

    Add files via upload

---
+++ WildCircus/src/test/java/com/wildCircus/demo/WildCircusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildCircus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildCircusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
